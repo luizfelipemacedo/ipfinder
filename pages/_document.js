c5ec06ac1459e4e1c54c8f50f1dde973c2d7c562
Luiz Macedo <felipemacedoc89@gmail.com>
2023-03-02 15:44:08 -0300

    .

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="mobile-web-app-capable" content="yes" />
+      </Head>
+      
       <body>
         <Main />
         <NextScript />
